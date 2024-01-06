8d5bbfd5c7634a99358f6e8f78225cb8925a0b1d
Ansh_R <138475625+Ansh-R-8@users.noreply.github.com>
2024-01-06 22:20:42 +0530

    Day_2.py

---
+++ coding_questions/Day_2.py
@@ -0,0 +1,28 @@
+'''Q Given a number N and an array of N strings,Print yes, if all strings have atleast one vowel in them otherwise print no.
+Input Size : N <= 1000
+Sample Testcase :
+INPUT
+5
+code
+overload
+vishal
+sundar
+anish
+OUTPUT
+yes
+'''
+
+# Solution:
+n = int(input())
+for i in range(n):
+    arr = [input()]
+Flag = True
+vowels = ['a','e','i','o','u']
+for char in arr:
+    if not any(let in vowels for let in char):
+        Flag = False
+        break
+if Flag:
+    print('yes')
+else:
+    print('no')
